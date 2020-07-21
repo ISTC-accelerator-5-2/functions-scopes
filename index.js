618bb1bf0b08f5d8122dccbe9d834207827b207b
Mikazakaryan <mikazakaryan94@gmail.com>
2020-07-21 12:34:35 +0400

    fucntion examples

---
+++ index.js
@@ -0,0 +1,16 @@
+// console.log(power(5));
+console.log(functionName(5, 6));
+
+function functionName(firstNumber, secondNumber) {
+  console.log(a);
+  if (true) {
+    var a = 5;
+    const b = 6;
+  }
+
+  return firstNumber * secondNumber;
+}
+
+const power = function (number) {
+  return number * number;
+};
